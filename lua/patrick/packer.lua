303b1d54e8da47fd911cef2e68f8598484199e41
Patrick <patrickwarley@gmail.com>
2024-03-15 20:46:43 -0300

    update

--- lua/patrick/packer.lua
+++ lua/patrick/packer.lua
@@ -13,48 +13,54 @@ vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
 
-return require('lazy').setup({
-    'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope.nvim',
-    'ThePrimeagen/vim-be-good',
-    'nvim-treesitter/playground',
-    'ThePrimeagen/harpoon',
-    'mbbill/undotree',
-    'tpope/vim-fugitive',
+return require("lazy").setup({
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "ThePrimeagen/vim-be-good",
+    "nvim-treesitter/playground",
+    "ThePrimeagen/harpoon",
+    "mbbill/undotree",
+    "tpope/vim-fugitive",
+    {
+        "roobert/tailwindcss-colorizer-cmp.nvim",
+        config = true
+    },
     {
         "rose-pine/neovim",
         name = "rose-pine",
         config = function()
-            vim.cmd('colorscheme rose-pine');
-        end
+            vim.cmd("colorscheme rose-pine")
+        end,
     },
     {
-        'nvim-treesitter/nvim-treesitter',
+        "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-    }, {
-    'nvim-treesitter/nvim-treesitter-context',
-    enabled = true
-},
+    },
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
+        "nvim-treesitter/nvim-treesitter-context",
+        enabled = true,
+    },
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v1.x",
         dependencies = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' },  -- Required
-            { 'williamboman/mason.nvim' }, -- Optional
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            { "neovim/nvim-lspconfig" },             -- Required
+            { "williamboman/mason.nvim" },           -- Optional
+            { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' }, -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'hrsh7th/cmp-buffer' }, -- Optional
-            { 'hrsh7th/cmp-path' }, -- Optional
-            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-            { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+            { "hrsh7th/nvim-cmp" },         -- Required
+            { "hrsh7th/cmp-nvim-lsp" },     -- Required
+            { "hrsh7th/cmp-buffer" },       -- Optional
+            { "hrsh7th/cmp-path" },         -- Optional
+            { "saadparwaiz1/cmp_luasnip" }, -- Optional
+            { "hrsh7th/cmp-nvim-lua" },     -- Optional
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' },  -- Required
-            { 'rafamadriz/friendly-snippets' }, -- Optional
-        }
+            { "L3MON4D3/LuaSnip" },             -- Required
+            { "rafamadriz/friendly-snippets" }, -- Optional
+        },
     },
+
 })
